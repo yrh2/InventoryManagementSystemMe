0f12f142885e58c8ebfbf5a881200c97c22f4e5d
yrh2 <syahirahas28@gmail.com>
2025-03-13 22:18:43 +0800

    Add files via upload

---
+++ InventoryManagementSystemMe/Controllers/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class InfoController : Controller
+{
+    public IActionResult AboutUs()
+    {
+        return View();
+    }
+
+    public IActionResult FAQ()
+    {
+        return View();
+    }
+}
+
